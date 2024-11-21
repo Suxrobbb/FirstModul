c67a567ba95b77be3ff956cd8bd40e7f39f16de0
Suxrobbb <nabiyevsuhrob.05@gmail.com>
2024-11-22 00:02:55 +0500

    homework

---
+++ 8 -dars uyga vazifa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8__dars_uyga_vazifa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
