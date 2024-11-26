875501bd38fc123079b700747d86b9b8b0e71502
Suxrobbb <nabiyevsuhrob.05@gmail.com>
2024-11-26 08:42:07 +0500

    Event service

---
+++ 9_dars uga vazifa/9-dars uyga vazifa/Services/EventService.cs
@@ -0,0 +1,133 @@
+﻿using _9_dars_uyga_vazifa.Models;
+
+namespace _9_dars_uyga_vazifa.Services;
+
+public class EventService
+{
+    private List<Event> ListedEvents;
+
+    public EventService()
+    {
+        ListedEvents = new List<Event>();
+        DataSeed();
+    }
+
+    public Event AddedEvent(Event addingEvent)
+    {
+        addingEvent.ID = Guid.NewGuid();
+        ListedEvents.Add(addingEvent);
+        return addingEvent;
+    }
+    public Event GetById(Guid eventId)
+    {
+        foreach (var chek in ListedEvents)
+        {
+            if (chek.ID == eventId)
+            {
+                return chek;
+            }
+        }
+        return null;
+    }
+    public bool DeletedEvent(Guid Id)
+    {
+        var deleteEvent = GetById(ListedEvents.ID);
+        if (deleteEvent is null)
+        {
+            return false;
+        }
+        ListedEvents.Remove(deleteEvent);
+        return true;
+    }
+    public bool UpdateEvent(Event eventId)
+    {
+        var eventDb = GetById(eventId.ID);
+        if (eventDb is null)
+        {
+            return false;
+        }
+        var index = ListedEvents.IndexOf(eventDb);
+        ListedEvents[index] = eventDb;
+        return true;
+    }
+    public List<Event> GetAllEvents()
+    {
+        return ListedEvents;
+    }
+    public void DisplayInfo(Event chek)
+    {
+        Console.WriteLine($"ID: {chek.ID}");
+        Console.WriteLine($"Title: {chek.Title}");
+        Console.WriteLine($"Location: {chek.Location}");
+        Console.WriteLine($"Date: {chek.Date}");
+        Console.WriteLine($"Description: {chek.Description}");
+        Console.WriteLine("Attendees: ");
+        foreach (var name in chek.Attendees)
+        {
+            Console.WriteLine($" - {name}");
+        }
+        Console.WriteLine("Tags: ");
+        foreach (var tag in chek.Tags)
+        {
+            Console.WriteLine($" - {tag}");
+        }
+    }
+    public List<Event> GetEventsByLocation(string location)
+    {
+        var eventsByLocation = new List<Event>();
+        foreach (var _event in ListedEvents)
+        {
+            if (_event.Location == location)
+            {
+                eventsByLocation.Add(_event);
+            }
+        }
+
+        return eventsByLocation;
+    }
+    /// Get Popular Event
+    public Event GetPopularEvent()
+    {
+        var mostPopularevent = new Event();
+        foreach (var eventItem in ListedEvents)
+        {
+            if (eventItem.Attendees.Count > mostPopularevent.Attendees.Count)
+            {
+                mostPopularevent = eventItem;
+            }
+        }
+
+        return mostPopularevent;
+    }
+    /// Get Max Tagged Event
+    public Event GetMaxTaggedEvent()
+    {
+        var mostTaggedEvent = new Event();
+        foreach (var eventItem in ListedEvents)
+        {
+            if (eventItem.Attendees.Count > mostTaggedEvent.Attendees.Count)
+            {
+                mostTaggedEvent = eventItem;
+            }
+        }
+
+        return mostTaggedEvent;
+    }
+    
+    public bool AddPersonToEvent(Guid Id, string person)
+    {
+        var EventFromDB = GetById(Id);
+        if (EventFromDB is null)
+        {
+            return false;
+        }
+        EventFromDB.Attendees.Add(person);
+
+        return true;
+    }
+
+}
+ 
+
+    
+
---
+++ 9_dars uga vazifa/9-dars uyga vazifa/Services/PostService.cs
@@ -0,0 +1,120 @@
+﻿using _9_dars_uyga_vazifa.Models
+
+namespace _9_dars_uyga_vazifa.Services;
+
+public class PostService
+{
+    private List<Post> posts;
+
+    public PostService()
+    {
+        posts = new List<Post>();
+    }
+
+    public Post AddPost(Post post)
+    {
+        post.Id = Guid.NewGuid();
+        posts.Add(post);
+
+        return post;
+    }
+
+    public Post GetPostById(Guid postId)
+    {
+        foreach (var post in posts)
+        {
+            if (post.Id==postId)
+            {
+                return post;
+            }
+        }
+        return null;
+    }
+    public bool DeletePost(Guid postId)
+    {
+        var postDb = GetPostById(postId);
+        if (postDb is null)
+        {
+            return false;
+        }
+        posts.Remove(postDb);
+        return true;
+    }
+    public bool UpdatePost(Post updatPost)
+    {
+        var postDb = GetPostById(updatPost.Id);
+        if (postDb is null)
+        {
+            return false;
+        }
+        var index = posts.IndexOf(postDb);
+        posts[index] = updatPost;
+        return true;
+    }
+    public List<Post> GetAllPosts()
+    {
+        return posts;
+    }
+
+    public Post GetMostViewedPost()
+    {
+        var mostViewedPost = new Post();
+        var mostView = 0;
+        foreach (var post in posts)
+        {
+            if (post.ViewerNames.Count>mostView)
+            {
+                mostView = post.ViewerNames.Count;
+                mostViewedPost = post;
+            }
+        }
+        return mostViewedPost;
+    }
+
+    public Post GetMostLikedPost()
+    {
+        Post mostLikeedPost = new Post();
+        var mostLiked = 0;
+        foreach (var post in posts)
+        {
+            if (post.QuantityLikes>mostLiked)
+            {
+                mostLiked = post.ViewerNames.Count;
+                mostLikeedPost=post;
+            }
+        }
+        return mostLikeedPost;
+    }
+
+    public Post GetMostCommentedPost()
+    {
+        Post mostCommentPost = new Post();
+        var mostComment = 0;
+        foreach (var post in posts)
+        {
+            if (post.Comments.Count>mostComment)
+            {
+                mostComment = post.Comments.Count;
+                mostCommentPost = post;
+            }
+        }
+        return mostCommentPost;
+    }
+
+    public List<Post> GetPostsByComment(string comment)
+    {
+        var mostComments = new List<Post>();
+        foreach (var post in posts)
+        {
+            var comments = post.Comments;
+            if (comment.Contains(comment) is true)
+            {
+                mostComments.Add(post);
+            }
+        }
+        return mostComments;
+
+    }
+
+}
+
